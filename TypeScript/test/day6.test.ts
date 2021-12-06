d794c234995ead505eec4de6aff48b207bc1e372
rolger <roland.germ@gmail.com>
2021-12-06 20:05:53 +0100

    fix: solve day 6

--- TypeScript/test/day6.test.ts
+++ TypeScript/test/day6.test.ts
@@ -15,6 +15,87 @@ class LaternFish {
     }
 }
 
+function extracted(initialFish: string[], days: number) {
+    let initFishCount = new Map();
+    initFishCount.set(0, initialFish[0].split(",").filter(f => f === "0").length);
+    initFishCount.set(1, initialFish[0].split(",").filter(f => f === "1").length);
+    initFishCount.set(2, initialFish[0].split(",").filter(f => f === "2").length);
+    initFishCount.set(3, initialFish[0].split(",").filter(f => f === "3").length);
+    initFishCount.set(4, initialFish[0].split(",").filter(f => f === "4").length);
+    initFishCount.set(5, initialFish[0].split(",").filter(f => f === "5").length);
+    initFishCount.set(6, initialFish[0].split(",").filter(f => f === "6").length);
+    initFishCount.set(7, initialFish[0].split(",").filter(f => f === "7").length);
+    initFishCount.set(8, initialFish[0].split(",").filter(f => f === "8").length);
+    initialFish = [];
+
+    let numberOfGens = new Map();
+    let fishBowl: LaternFish[] = [];
+    let fishCount = new Map();
+    for (const age of [0, 1, 2, 3, 4, 5, 6, 7, 8]) {
+        fishBowl = [new LaternFish(age)];
+
+        let newGeneration: LaternFish[] = [];
+        for (let i = 0; i < days; i++) {
+            for (const fish of fishBowl) {
+                newGeneration.push(...(fish.nextDay()));
+            }
+            for (const next of newGeneration) {
+                fishBowl.push(next);
+            }
+
+            newGeneration = [];
+        }
+        numberOfGens.set(age, {
+         total:   fishBowl.length,
+         part0: countFish( 0, fishBowl),
+         part1: countFish( 1, fishBowl),
+         part2: countFish( 2, fishBowl),
+         part3: countFish( 3, fishBowl),
+         part4: countFish( 4, fishBowl),
+         part5: countFish( 5, fishBowl),
+         part6: countFish( 6, fishBowl),
+         part7: countFish( 7, fishBowl),
+         part8: countFish( 8, fishBowl)
+        });
+        fishBowl = [];
+    }
+    return {initFishCount, numberOfGens, fishCount};
+}
+
+function countFish(key: number, fishes: LaternFish[]) {
+    return fishes.filter(f => f.age === key).length;
+}
+
+function countGenerations(initialFish: string[], days: number) {
+    let fishCount = new Map();
+    fishCount.set(0, initialFish[0].split(",").filter(f => f === "0").length);
+    fishCount.set(1, initialFish[0].split(",").filter(f => f === "1").length);
+    fishCount.set(2, initialFish[0].split(",").filter(f => f === "2").length);
+    fishCount.set(3, initialFish[0].split(",").filter(f => f === "3").length);
+    fishCount.set(4, initialFish[0].split(",").filter(f => f === "4").length);
+    fishCount.set(5, initialFish[0].split(",").filter(f => f === "5").length);
+    fishCount.set(6, initialFish[0].split(",").filter(f => f === "6").length);
+    fishCount.set(7, initialFish[0].split(",").filter(f => f === "7").length);
+    fishCount.set(8, initialFish[0].split(",").filter(f => f === "8").length);
+
+    for (let i = 0; i < days; i++) {
+        let temp = fishCount.get(0);
+
+        for (let j = 0; j <= 5; j++) {
+            fishCount.set(j, fishCount.get(j + 1));
+        }
+        fishCount.set(6, temp + fishCount.get(7));
+        fishCount.set(7, fishCount.get(8));
+        fishCount.set(8, temp);
+    }
+
+    let numOfFish = 0;
+    for (let i = 0; i <= 8; i++) {
+        numOfFish = numOfFish + fishCount.get(i);
+    }
+    return numOfFish;
+}
+
 describe('Day6 Test', () => {
 
     it('should decrease age by 1', () => {
@@ -78,55 +159,20 @@ describe('Day6 Test', () => {
         expect(fishes.length).toBe(5934);
     });
 
-    function countFish(countOf: Map<number, number>, key: number, fishes: LaternFish[]) {
-        let oldVal = countOf.get(key) ?? 0;
-        countOf.set(key, fishes.filter(f => f.age === key).length + oldVal);
-        return countOf;
-    }
 
-    it('should handle file input', () => {
-        let initialFish = readInput(6);
+    it('should count generations', () => {
+        // let initialFish = readInput(6);
+        let initialFish = ["3,4,3,1,2"];
 
-        /*
-        let fishes: LaternFish[] = [];
-        for (const age of initialFish[0].split(",")) {
-            fishes.push(new LaternFish(Number.parseInt(age)));
-        }
-        */
-
-        let numberOfGenerationsOf = new Map();
-        let fishCount = new Map();
-        for (const age of [1, 2, 3, 4, 5, 6, 7, 8]) {
-            let currentFish = [new LaternFish(age)];
-
-            let newGeneration: LaternFish[] = [];
-            for (let i = 0; i < 64; i++) {
-                for (const fish of currentFish) {
-                    newGeneration.push(...(fish.nextDay()));
-                }
-                for (const next of newGeneration) {
-                    currentFish.push(next);
-                }
-            }
-            numberOfGenerationsOf.set(age, currentFish.length);
-/*
-            fishCount = countFish(fishCount, 1, currentFish);
-            fishCount = countFish(fishCount, 2, currentFish);
-            fishCount = countFish(fishCount, 3, currentFish);
-            fishCount = countFish(fishCount, 4, currentFish);
-            fishCount = countFish(fishCount, 5, currentFish);
-            fishCount = countFish(fishCount, 6, currentFish);
-            fishCount = countFish(fishCount, 7, currentFish);
-            fishCount = countFish(fishCount, 8, currentFish);
-
- */
-        }
-
-        console.log(numberOfGenerationsOf);
-        console.log(fishCount);
-
-        //expect(fishes.length).toBe(379114);
+        expect(countGenerations(initialFish, 80)).toBe(5934);
+        expect(countGenerations(initialFish, 256)).toBe(26984457539);
     });
 
+    it('should count generations from file', () => {
+        let initialFish = readInput(6);
 
+        expect(countGenerations(initialFish, 256)).toBe(1702631502303);
+    });
 });
+
+
