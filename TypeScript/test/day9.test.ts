19c213f20c4ff5436bb5d079722bd186883c6f27
rolger <roland.germ@gmail.com>
2022-01-17 22:34:43 +0100

    refactor: structure tests for lowest location

--- TypeScript/test/day9.test.ts
+++ TypeScript/test/day9.test.ts
@@ -79,8 +79,8 @@ function toIntArray(line: string | undefined) {
 
 describe('Day9 Test', () => {
 
-    describe('check first row', () => {
-        it('should find lowest', () => {
+    describe('should find lowest locations', () => {
+        it('in first row', () => {
             let low = lowestLocation([], [2, 1, 9], [3, 9, 8]);
             expect(low).to.eql([1]);
             low = lowestLocation([], [1, 2, 9], [3, 9, 8]);
@@ -88,14 +88,13 @@ describe('Day9 Test', () => {
             low = lowestLocation([], [9, 2, 1], [8, 9, 3]);
             expect(low).to.eql([1]);
         });
-        it('should find lowest more digits', () => {
+
+        it('in first row with multiple instances', () => {
             let low = lowestLocation([], toIntArray("2199943210"), toIntArray("3987894921"));
             expect(low).to.eql([1, 0]);
         });
-    });
 
-    describe('check last row', () => {
-        it('should find lowest', () => {
+        it('in last row', () => {
             let low = lowestLocation([9, 6, 7], [6, 5, 6], []);
             expect(low).to.eql([5]);
             low = lowestLocation([9, 6, 7], [4, 5, 6], []);
@@ -103,10 +102,8 @@ describe('Day9 Test', () => {
             low = lowestLocation([9, 6, 7], [6, 5, 3], []);
             expect(low).to.eql([3]);
         });
-    });
 
-    describe('check middle row', () => {
-        it('should find lowest', () => {
+        it('in middle row', () => {
             let low = lowestLocation([9, 8, 7], [8, 5, 6], [7, 6, 7]);
             expect(low).to.eql([5]);
             low = lowestLocation([9, 8, 7], [3, 5, 6], [7, 6, 7]);
@@ -114,31 +111,31 @@ describe('Day9 Test', () => {
             low = lowestLocation([9, 8, 7], [9, 5, 1], [7, 6, 7]);
             expect(low).to.eql([1]);
         });
-    });
 
-    it('should find lowest locations in matrix', () => {
-        let matrix = [
-            "2199943210",
-            "3987894921",
-            "9856789892",
-            "8767896789",
-            "9899965678"
-        ];
-        let lowestLocations = findLowestLocations(matrix);
+        it('in matrix', () => {
+            let matrix = [
+                "2199943210",
+                "3987894921",
+                "9856789892",
+                "8767896789",
+                "9899965678"
+            ];
+            let lowestLocations = findLowestLocations(matrix);
 
-        expect(lowestLocations).to.eql([1, 0, 5, 5]);
-        let sumRisk = lowestLocations.map(i => i + 1)
-            .reduce((a, b) => a + b);
-        expect(sumRisk).to.equal(15);
-    });
+            expect(lowestLocations).to.eql([1, 0, 5, 5]);
+            let sumRisk = lowestLocations.map(i => i + 1)
+                .reduce((a, b) => a + b);
+            expect(sumRisk).to.equal(15);
+        });
 
-    it('should find lowest locations in file', () => {
-        let input = readInput(9);
-        let lowestLocations = findLowestLocations(input);
+        it('in file', () => {
+            let input = readInput(9);
+            let lowestLocations = findLowestLocations(input);
 
-        let sumRisk = lowestLocations.map(i => i + 1)
-            .reduce((a, b) => a + b);
-        expect(sumRisk).to.equal(425);
+            let sumRisk = lowestLocations.map(i => i + 1)
+                .reduce((a, b) => a + b);
+            expect(sumRisk).to.equal(425);
+        });
     });
 
 });
