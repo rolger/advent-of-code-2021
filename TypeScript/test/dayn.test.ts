4fa41374d582ea87b0dc3ee4fd845856df5959ce
rgerm <roland.germ@admiral.at>
2022-12-04 22:12:13 +0100

    feature: update template with stream

--- TypeScript/test/dayn.test.ts
+++ TypeScript/test/dayn.test.ts
@@ -1,12 +1,38 @@
+import {readInput} from "./read-adventofcode-input";
+
 describe('DayN Test', () => {
+
+    function sum(): (a: number, b: number) => number {
+        return (a: number, b: number) => a + b;
+    }
 
     describe('1st Puzzle', () => {
         it('should', () => {
+        });
+
+        xit('should ... from file', () => {
+            let input = readInput(-1);
+
+            let result = input
+                .map(s => Number.parseInt(s))
+                .reduce(sum(), 0);
+
+            expect(result).toBe(0);
         });
     });
 
     describe('2nd Puzzle', () => {
         it('should', () => {
+        });
+
+        xit('should ... from file', () => {
+            let input = readInput(-1);
+
+            let result = input
+                .map(s => Number.parseInt(s))
+                .reduce(sum(), 0);
+
+            expect(result).toBe(0);
         });
     });
 
