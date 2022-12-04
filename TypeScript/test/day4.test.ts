f6703c0a7cd40d9bf8829d696016e0d6bddd7524
rgerm <roland.germ@admiral.at>
2022-12-04 12:09:12 +0100

    feature: prepare day 4

---
+++ TypeScript/test/day4.test.ts
@@ -0,0 +1,13 @@
+describe('Day4 Test', () => {
+
+    describe('1st Puzzle', () => {
+        it('should', () => {
+        });
+    });
+
+    describe('2nd Puzzle', () => {
+        it('should', () => {
+        });
+    });
+
+});
