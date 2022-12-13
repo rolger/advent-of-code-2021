b5580e973aee00f4f35a3b105c31c5e4867f2778
patrickscheibert <patrick.scheibert@outlook.com>
2022-12-13 19:04:03 +0100

    test rock against paper loses

--- Java/src/test/java/adventofcode/AdventOfCodeDay1.java
+++ Java/src/test/java/adventofcode/AdventOfCodeDay1.java
@@ -4,22 +4,31 @@ package adventofcode;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 // https://adventofcode.com/2022
 class AdventOfCodeDay1 {
 
-    @Test
-    void twoShapesAreSame() {
-        assertThat(playRockPaperScissor("A", "X")).isEqualTo(4);
-        assertThat(playRockPaperScissor("B", "Y")).isEqualTo(5);
-        assertThat(playRockPaperScissor("C", "Z")).isEqualTo(6);
+    @ParameterizedTest
+    @CsvSource(value = {"A,X,4", "B,Y,5", "C,Z,6"})
+    void twoShapesAreSame(String myShape, String otherShape, int expectedScore) {
+        assertThat(playRockPaperScissor(myShape, otherShape)).isEqualTo(expectedScore);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"A,Y,1"})
+    void myShapeLoses(String myShape, String otherShape, int expectedScore) {
+        assertThat(playRockPaperScissor(myShape, otherShape)).isEqualTo(expectedScore);
     }
 
     private int playRockPaperScissor(String myShape, String otherShape) {
         if (myShape.equals("B")) {
             return 5;
+        } else if (myShape.equals("C")) {
+            return 6;
         }
         return 4;
     }
