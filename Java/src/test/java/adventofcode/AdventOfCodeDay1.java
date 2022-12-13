319b902fcb49cf693ce25894af1dd59f8cd8500b
rgerm <roland.germ@admiral.at>
2022-12-13 18:56:03 +0100

    test: compare two scissors

--- Java/src/test/java/adventofcode/AdventOfCodeDay1.java
+++ Java/src/test/java/adventofcode/AdventOfCodeDay1.java
@@ -5,20 +5,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 // https://adventofcode.com/2022
 class AdventOfCodeDay1 {
 
     @Test
-    void twoRocksAreA4() {
-        Assertions.assertThat(playRockPaperScissor("A", "X")).isEqualTo(4);
-    }
-
-    @Test
-    void twoPaperAreA5() {
-        Assertions.assertThat(playRockPaperScissor("B", "Y")).isEqualTo(5);
+    void twoShapesAreSame() {
+        assertThat(playRockPaperScissor("A", "X")).isEqualTo(4);
+        assertThat(playRockPaperScissor("B", "Y")).isEqualTo(5);
+        assertThat(playRockPaperScissor("C", "Z")).isEqualTo(6);
     }
 
     private int playRockPaperScissor(String myShape, String otherShape) {
+        if (myShape.equals("B")) {
+            return 5;
+        }
         return 4;
     }
 
