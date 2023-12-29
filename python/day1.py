2949f72781d1cc799c4542005fbcd778ffe7a830
rgerm <roland.germ@gmail.com>
2023-12-29 15:20:00 +0100

    fix: add more python

--- python/day1.py
+++ python/day1.py
@@ -48,5 +48,4 @@ if __name__ == "__main__":
     args = sys.argv
     # args[0] = current file
     # args[1] = function name
-    # args[2:] = function args : (*unpacked)
-    globals()[args[1]](*args[2:])
+    globals()[args[1]]()
